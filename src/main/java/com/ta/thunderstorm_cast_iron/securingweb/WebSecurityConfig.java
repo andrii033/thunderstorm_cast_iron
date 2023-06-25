8299c438f729078cfd9ca4de2ea5657d5bf7929c
andrii <ataran033@gmail.com>
2023-06-25 13:10:50 +0200

    code

--- src/main/java/com/ta/thunderstorm_cast_iron/securingweb/WebSecurityConfig.java
+++ src/main/java/com/ta/thunderstorm_cast_iron/securingweb/WebSecurityConfig.java
@@ -4,42 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.ta.thunderstorm_cast_iron.model.entity.Users;
+
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfiguration{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/hello").hasRole("USER")
-                        .requestMatchers("/", "/home","/registration").permitAll()
-                        .anyRequest().authenticated()
-                )
+                        .requestMatchers("/", "/home", "/registration").permitAll()
+                        .anyRequest().authenticated())
                 .formLogin((form) -> form
                         .loginPage("/login")
-                        .permitAll()
-                )
+                        .permitAll())
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("123")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
+    
 }
